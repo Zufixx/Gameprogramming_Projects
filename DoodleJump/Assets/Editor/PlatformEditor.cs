349c055be6fe3d0541bdde8d8130652ae099e524
Filip Bergkvist <filip.0717@hotmail.com>
2017-11-21 00:18:59 +0100

    Type is now called PlatformType, moved code to methods, trying to add customizable types

--- DoodleJump/Assets/Editor/PlatformEditor.cs
+++ DoodleJump/Assets/Editor/PlatformEditor.cs
@@ -8,7 +8,7 @@ using System.IO;
 public class PlatformEditor : Editor
 {
     [SerializeField]
-    public List<Type> Types;
+    public List<PlatformType> Types;
 
     // Variables for creating a new type
     private int new_id;
@@ -21,8 +21,9 @@ public class PlatformEditor : Editor
 
     private void Awake()
     {
-        Types = new List<Type>();
+        Types = new List<PlatformType>();
 
+        // Set all initial inspector values
         new_id = Types.Count + 1;
         new_typeName = "Temp";
         new_jumpHeight = 10f;
@@ -31,6 +32,22 @@ public class PlatformEditor : Editor
         new_speed = Vector2.zero;
         new_time = 0.5f;
 
+        ReadFile();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        PlatformCreator platformCreator = (PlatformCreator)target;
+
+        base.OnInspectorGUI();
+
+        DrawNewType();
+        DrawCurrentTypes();
+        //DrawRemoveType();
+    }
+
+    private void ReadFile()
+    {
         int i = 0;
 
         string contents = string.Empty;
@@ -48,7 +65,7 @@ public class PlatformEditor : Editor
                 {
                     string[] value = line.Split(new char[] { ',' });
 
-                    Type type = new Type(
+                    PlatformType platformType = new PlatformType(
                         i,                                  // ID (int)
                         value[0].Trim(),                    // TypeName (string)
                         float.Parse(value[1].TrimEnd()),    // JumpHeight
@@ -60,44 +77,28 @@ public class PlatformEditor : Editor
                         float.Parse(value[7].TrimEnd()),    // MovementY
                         float.Parse(value[8].TrimEnd())     // Time
                         );
-                    Types.Add(type);
+                    Types.Add(platformType);
                     i++;
                 }
             }
         }
     }
 
-    public override void OnInspectorGUI()
+    private void DrawNewType()
     {
-        PlatformCreator platformCreator = (PlatformCreator)target;
-
-        base.OnInspectorGUI();
-
-        EditorGUILayout.LabelField("Current types: ", EditorStyles.boldLabel);
-        for (int i = 0; i < Types.Count; i++)
-        {
-            EditorGUILayout.LabelField("Type ID: " + Types[i].GetID());
-            EditorGUILayout.LabelField("Type Name: " + Types[i].GetTypeName());
-            EditorGUILayout.ColorField("Color: ", Types[i].GetColor());
-            EditorGUILayout.LabelField("Probability: " + Types[i].GetProbability() + "%");
-            EditorGUILayout.Vector2Field("Movement: ", Types[i].GetMovement());
-            EditorGUILayout.LabelField("Time: " + Types[i].GetTime() + "s");
-            EditorGUILayout.Space();
-        }
-        EditorGUILayout.LabelField("");
         EditorGUILayout.LabelField("Add new type: ", EditorStyles.boldLabel);
         EditorGUILayout.LabelField("Type ID: " + new_id);
-        EditorGUILayout.TextField("Type name:", new_typeName);
-        EditorGUILayout.ColorField("Color: ", new_color);
-        EditorGUILayout.FloatField("Jump height: ", new_jumpHeight);
-        EditorGUILayout.Slider("Probability (%):", new_probability, 0f, 100f);
+        new_typeName = EditorGUILayout.TextField("Type name:", new_typeName);
+        new_color = EditorGUILayout.ColorField("Color: ", new_color);
+        new_jumpHeight = EditorGUILayout.FloatField("Jump height: ", new_jumpHeight);
+        new_probability = EditorGUILayout.Slider("Probability (%):", new_probability, 0f, 100f);
 
+        EditorGUILayout.LabelField("");
         EditorGUILayout.Space();
+
         EditorGUILayout.LabelField("Movement:");
-        EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.Vector2Field("Speed: ", new_speed);
-        EditorGUILayout.EndHorizontal();
-        EditorGUILayout.FloatField("Time (s): ", new_time);
+        new_speed = EditorGUILayout.Vector2Field("Speed: ", new_speed);
+        new_time = EditorGUILayout.FloatField("Time (s): ", new_time);
 
         EditorGUILayout.Space();
 
@@ -105,9 +106,41 @@ public class PlatformEditor : Editor
         {
             Debug.Log(new_typeName);
         }
+    }
 
-        EditorGUILayout.Space();
+    private void DrawCurrentTypes()
+    {
         EditorGUILayout.LabelField("");
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Current types: ", EditorStyles.boldLabel);
+        for (int i = 0; i < Types.Count; i++)
+        {
+            Types[i].id = EditorGUILayout.IntField("Type ID: ", Types[i].id);
+            Types[i].typeName = EditorGUILayout.TextField("Type Name: ", Types[i].typeName);
+            Types[i].color = EditorGUILayout.ColorField("Color: ", Types[i].color);
+            Types[i].probability = EditorGUILayout.FloatField("Probability: ", Types[i].probability);
+            Types[i].speed = EditorGUILayout.Vector2Field("Movement: ", Types[i].speed);
+            Types[i].time = EditorGUILayout.FloatField("Time: ", Types[i].time);
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("Save values"))
+            {
+                Debug.Log("Save values");
+            }
+            if (GUILayout.Button("Delete type"))
+            {
+                Debug.Log("Deleted type");
+            }
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.LabelField("");
+            EditorGUILayout.Space();
+        }
+    }
+
+    private void DrawRemoveType()
+    {
+        EditorGUILayout.LabelField("");
+        EditorGUILayout.Space();
 
         EditorGUILayout.LabelField("Remove a type:", EditorStyles.boldLabel);
         EditorGUILayout.TextField("Type name: ", "");
@@ -118,20 +151,23 @@ public class PlatformEditor : Editor
     }
 }
 
-public class Type : MonoBehaviour
+public class PlatformType : MonoBehaviour
 {
-    int id;
-    string typeName;
-    float jumpHeight;
-    float colorR;
-    float colorG;
-    float colorB;
-    float probability;
-    float movementX;
-    float movementY;
-    float time;
+    public int id;
+    public string typeName;
+    public float jumpHeight;
+    public float colorR;
+    public float colorG;
+    public float colorB;
+    public float probability;
+    public float movementX;
+    public float movementY;
+    public float time;
+
+    public Color color;
+    public Vector2 speed;
     
-    public Type(int id, string typeName, float jumpHeight, float colorR, float colorG, float colorB, float probability, float movementX, float movementY, float time)
+    public PlatformType(int id, string typeName, float jumpHeight, float colorR, float colorG, float colorB, float probability, float movementX, float movementY, float time)
     {
         this.id = id;
         this.typeName = typeName;
@@ -143,13 +179,9 @@ public class Type : MonoBehaviour
         this.movementX = movementX;
         this.movementY = movementY;
         this.time = time;
-    }
 
-    public int GetID() { return id; }
-    public string GetTypeName() { return typeName; }
-    public float GetJumpHeight() { return jumpHeight; }
-    public Color GetColor() { return new Color(colorR, colorG, colorB); }
-    public float GetProbability() { return probability; }
-    public Vector2 GetMovement() { return new Vector2(movementX, movementY); }
-    public float GetTime() { return time; }
+        color = new Color(colorR, colorG, colorB);
+        speed = new Vector2(movementX, movementY);
+
+    }
 }

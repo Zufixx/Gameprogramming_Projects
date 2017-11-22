60525f33a85e61b71f653832564c2c911dde970d
Zufixx <filip.0717@hotmail.com>
2017-11-22 21:23:17 +0100

    Player is now more customizable and Added Powerups

--- DoodleJump/Assets/Scripts/PlayerController.cs
+++ DoodleJump/Assets/Scripts/PlayerController.cs
@@ -5,129 +5,151 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
     private float moveHorizontal;
 
-    public float speed;
-    public float jumpHeight;
+    [SerializeField]
+    private float speed;
+    private float jumpHeight;
 
     private Vector2 rayOrigin;
     private Vector2 rayDirection;
     private float rayDistance;
 
-    [SerializeField]
     private float maxHeight;
-
-    [SerializeField]
-    private PlatformManager platformManager;
 
     [SerializeField]
     private GameObject bullet;
 
+    [Header("PowerUp Settings")]
+    public bool powerUp = false;
+    [SerializeField]
+    private float powerUpTimer = 3f;
+    private float powerUpTimerStart;
+
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        powerUpTimerStart = powerUpTimer;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        #region Movement
-        // Movement forces
-        moveHorizontal = Input.GetAxisRaw("Horizontal");
-        Vector2 movement = new Vector2(moveHorizontal, 0.0f);
-        rb.AddForce(movement * speed);
-        #endregion
+        if (!GameManager.instance.gameOver)
+        {
+            // Movement forces
+            moveHorizontal = Input.GetAxisRaw("Horizontal");
+            Vector2 movement = new Vector2(moveHorizontal, 0.0f);
+            rb.AddForce(movement * speed);
 
-        #region Character Flipping
-        // Flip character when moving side to side
-        if (Input.GetAxisRaw("Horizontal") > 0.1f)
-        {
-            gameObject.transform.localRotation = new Quaternion(0f, 180f, 0f, 0f);
-        }
-        else if (Input.GetAxisRaw("Horizontal") < -0.1f)
-        {
-            gameObject.transform.localRotation = new Quaternion(0f, 0f, 0f, 0f);
-        }
-        #endregion
+            // Flip character when moving side to side
+            if (Input.GetAxisRaw("Horizontal") > 0.1f)
+            {
+                gameObject.transform.localRotation = new Quaternion(0f, 180f, 0f, 0f);
+            }
+            else if (Input.GetAxisRaw("Horizontal") < -0.1f)
+            {
+                gameObject.transform.localRotation = new Quaternion(0f, 0f, 0f, 0f);
+            }
 
-        #region Teleporting
-        // Teleport from side to side
-        if (transform.position.x >= 3.15f)
-        {
-            transform.position = new Vector2(-3.14f, transform.position.y);
-        }
-        else if (transform.position.x <= -3.15f)
-        {
-            transform.position = new Vector2(3.14f, transform.position.y);
-        }
-        #endregion
+            // Teleport from side to side
+            if (transform.position.x >= 3.15f)
+            {
+                transform.position = new Vector2(-3.14f, transform.position.y);
+            }
+            else if (transform.position.x <= -3.15f)
+            {
+                transform.position = new Vector2(3.14f, transform.position.y);
+            }
 
-        #region Max Height
-        // Set max height
-        if (maxHeight < transform.position.y)
-            maxHeight = transform.position.y;
-        #endregion
+            // Set max height
+            if (maxHeight < transform.position.y)
+                maxHeight = transform.position.y;
 
-        #region Shooting
-        if(Input.GetButtonDown("Fire1"))
-        {
-            Instantiate(bullet, transform.position, Quaternion.identity);
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Instantiate(bullet, transform.position, Quaternion.identity);
+            }
         }
-        #endregion
+
+        if(powerUp && powerUpTimer >= 0f)
+        {
+            rb.AddForce(new Vector2(0f, 15f));
+            powerUpTimer -= Time.deltaTime;
+            sr.color = Color.red;
+        }
+        else if (powerUpTimer < 0f)
+        {
+            powerUp = false;
+            powerUpTimer = powerUpTimerStart;
+            sr.color = Color.white;
+        }
     }
 
     private void FixedUpdate()
     {
-        #region Raycasting
-        // Raycasting variables
-        rayOrigin = transform.position;
-        //rayOrigin = transform.position - new Vector3(0.0f, 0.4f, 0.0f);
-        rayDirection = -Vector2.up;
-        rayDistance = 0.42f;
-        LayerMask layer = 1 << 0;
-
-        // Shoot ray down from player
-        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, rayDirection, rayDistance, layer);
-
-        // Debug Drawing
-        Color color = hit ? Color.green : Color.red;
-        Debug.DrawRay(rayOrigin, rayDirection, color);
-
-        //Debug.Log(hit);
-
-        // If it hits a platform
-        if (hit.collider != null && rb.velocity.y <= 0.0f)
+        if (!GameManager.instance.gameOver)
         {
-            Platform platform = hit.transform.gameObject.GetComponent<Platform>();
+            // Raycasting variables
+            rayOrigin = transform.position;
+            rayDirection = -Vector2.up;
+            rayDistance = 0.42f;
+            LayerMask layer = 1 << 0;
 
-            if (platform.fragile && platform.jumpHeight <= 0f)
+            // Shoot ray down from player
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, rayDirection, rayDistance, layer);
+
+            // Debug Drawing
+            Color color = hit ? Color.green : Color.red;
+            Debug.DrawRay(rayOrigin, rayDirection, color);
+
+            // If it hits a platform
+            if (hit.collider != null && rb.velocity.y <= 0.0f)
             {
-                Destroy(platform.gameObject);
-                return;
-            }
-            else if (platform.fragile)
-            {
-                // Jump with the height set on the platform
-                jumpHeight = platform.GetJumpHeight();
-                rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+                if (hit.transform.tag == "Platform" || hit.transform.tag == "Ground")
+                {
+                    Platform platform = hit.transform.gameObject.GetComponent<Platform>();
 
-                Destroy(platform.gameObject);
-                return;
-            }
-            else
-            {
-                // Jump with the height set on the platform
-                jumpHeight = platform.GetJumpHeight();
-                rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
-            }
+                    if (platform.fragile && platform.jumpHeight <= 0f)
+                    {
+                        Destroy(platform.gameObject);
+                        return;
+                    }
+                    else if (platform.fragile)
+                    {
+                        // Jump with the height set on the platform
+                        jumpHeight = platform.GetJumpHeight();
+                        rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
 
-
-            Debug.Log("A hit!");
+                        Destroy(platform.gameObject);
+                        return;
+                    }
+                    else
+                    {
+                        // Jump with the height set on the platform
+                        jumpHeight = platform.GetJumpHeight();
+                        rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+                    }
+                }
+            }
         }
-        #endregion
     }
 
-    public float GetMaxHeight()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        return maxHeight;
+        if (collision.transform.tag == "Enemy" && !powerUp)
+        {
+            GameManager.instance.GameOver();
+            sr.enabled = false;
+        }
+        else if (collision.transform.tag == "Powerup")
+        {
+            //Debug.Log("POWER-UP");
+            powerUp = true;
+            Destroy(collision.transform.gameObject);
+        }
     }
+
+    public float GetMaxHeight() { return maxHeight; }
 }
---
+++ DoodleJump/Assets/Scripts/Powerup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour {
+
+    private Vector2 startPosition;
+    [SerializeField]
+    private float bounceSpeed = 5f;
+    [SerializeField]
+    private float bounceAmount = 10f;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = startPosition + new Vector2(0.0f, Mathf.Sin(Time.time * bounceSpeed) / bounceAmount);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.transform.tag == "Boundary")
+            Destroy(gameObject);
+    }
+}

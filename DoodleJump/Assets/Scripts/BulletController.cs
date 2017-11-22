e676ed38424b8474d5c5be049ae37313f898a23c
Zufixx <filip.0717@hotmail.com>
2017-11-22 21:19:40 +0100

    Bullets now check for collisions and destroy enemies

--- DoodleJump/Assets/Scripts/BulletController.cs
+++ DoodleJump/Assets/Scripts/BulletController.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.transform.tag == "Boundary")
             Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
     }
 }

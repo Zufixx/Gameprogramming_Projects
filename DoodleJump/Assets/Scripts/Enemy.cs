77bb3636d1bdeaaa17a80f713098207961202104
Zufixx <filip.0717@hotmail.com>
2017-11-22 21:20:45 +0100

    Added enemy behaviour

---
+++ DoodleJump/Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+	
+	void Update () {
+        float moveY = transform.position.y + -Time.deltaTime * speed;
+        transform.position = new Vector2(transform.position.x, moveY);
+	}
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.transform.tag == "Boundary")
+            Destroy(gameObject);
+    }
+}

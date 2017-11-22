31ec6d766819e0828df3c862eab2f8ac1c23679a
Zufixx <filip.0717@hotmail.com>
2017-11-22 21:20:33 +0100

    Cleaned up CameraController

--- DoodleJump/Assets/Scripts/CameraController.cs
+++ DoodleJump/Assets/Scripts/CameraController.cs
@@ -12,24 +12,17 @@ public class CameraController : MonoBehaviour {
 
     private void FixedUpdate()
     {
-        #region Distance to Max Height
         distance = playerController.GetMaxHeight() - transform.position.y + 1.0f;
 
         if (distance > 0)
         {
             transform.Translate(new Vector3(0.0f, distance / 10f, 0.0f), Space.Self);
         }
-        #endregion
 
-        #region Game Over if fall off
         trueDistance = playerController.transform.position.y - transform.position.y;
         if (trueDistance < -5)
             GameManager.instance.GameOver();
-        #endregion
     }
 
-    public float GetDistance()
-    {
-        return distance;
-    }
+    public float GetDistance() { return distance; }
 }

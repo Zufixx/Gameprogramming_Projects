294b8f10e9bf3bf1b0b562a2683b564c634c546e
Zufixx <filip.0717@hotmail.com>
2017-12-14 23:00:08 +0100

    Add GameManager to handle game overs

---
+++ Legend of Zelda/Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+
+    public bool gameOver = false;
+
+    [SerializeField]
+    private Text healthText;
+    [SerializeField]
+    private GameObject player;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+	
+	void Update () {
+		if(gameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+	}
+
+    public void GameOver()
+    {
+        gameOver = true;
+        //Debug.Log("Game Over");
+        healthText.text = "Game Over!";
+        player.SetActive(false);
+    }
+}
